7421b18c16c9f8b1eafe09dca4651a4bb5cf8e2b
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-08-03 00:59:58 +0300

    Implement dark mode feature with theme switch and styling updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+let darkmode = localStorage.getItem('darkmode');7
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem("darkmode", "active")
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem("darkmode", null)
+}
+
+if (darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+});
 const fact = document.getElementById("fact");
 const factButton = document.getElementById('nw-fact');
 
